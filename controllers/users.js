a3088ff13cd070a6b333a3cb0b3a673aac818291
Vladislav Sakharov <brodron@gmail.com>
2020-04-16 22:49:50 +0300

    cors update

--- controllers/users.js
+++ controllers/users.js
@@ -67,7 +67,7 @@ module.exports.login = (req, res, next) => {
             const token = jwt.sign({
               _id: data._id,
             }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-            res.send({ token });
+            res.send({ data, token });
           } else {
             try {
               throw new UnauthorizedError('Ошибка авторизации');
